e941c0038cf3bb98d63b4d83b56eef99f301f304
Евгений <jenek95@mail.ru>
2021-07-10 11:48:46 +0300

    start slider

--- build/js/main.js
+++ build/js/main.js
@@ -149,3 +149,27 @@ if (links) {
     });
   };
 }
+
+const swiper = new Swiper('.swiper-container', {
+  speed: 400,
+  spaceBetween: 30,
+  loop: true,
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '"> ' + (index + 1) + '</span>';
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
